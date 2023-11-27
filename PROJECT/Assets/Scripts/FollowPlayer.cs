79113a9d41657d1d83c18b2d5b5fa46ad857048f
ChoiOnion <117707375+ChoiOnion@users.noreply.github.com>
2023-11-27 19:31:39 +0900

    플레이어 이동 키보드로 변경, 적 소멸 구현

---
+++ PROJECT/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position+new Vector3 (-5.0f, 3.0f, -5.0f);
+    }
+}
--- Project/Assets/Player.cs
+++ Project/Assets/Player.cs
@@ -22,7 +22,6 @@ public class Player : MonoBehaviour
 
     int tempOffense;
 
-    bool move = false;
 
     private void Start()
     {
@@ -31,47 +30,29 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        // 왼쪽 마우스 버튼을 눌렀을 때
-        if (Input.GetMouseButtonDown(0))
+        float moveZ = 0f;
+        float moveX = 0f;
+        if (Input.GetKey(KeyCode.W))
         {
-            RaycastHit hit;
-            // 메인 카메라를 통해 마우스 클릭한 곳의 ray 정보를 가져옴
-            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            // ray와 닿은 물체가 있는지 검사
-            if (Physics.Raycast(ray, out hit, 100f))
-            {
-                print(hit.transform.name);
-
-                // hit.point 는 마우스 클릭한 곳의 월드 좌표.
-                // 이 예제에서 캐릭터의 y 값(높이) 은 변하지 않기 때문에 
-                // 아래와 같이 목표위치를 재설정합니다.
-                destPos = new Vector3(hit.point.x, transform.position.y, hit.point.z);
-
-                // 현재 위치와 목표 위치의 방향 벡터
-                dir = destPos - transform.position;
-
-                // 바라 보아야 할 곳의 Quaternion
-                lookTarget = Quaternion.LookRotation(dir);
-                move = true;
-            }
+            moveZ += 1f;
         }
 
-        Move();
-    }
-
-    void Move()
-    {
-        if (move)
+        if (Input.GetKey(KeyCode.S))
         {
-            // 이동할 방향으로 Time.deltaTime * 2f 의 속도로 움직임.
-            transform.position += dir.normalized * Time.deltaTime * 2f;
-            // 현재 방향에서 움직여야할 방향으로 부드럽게 회전
-            transform.rotation = Quaternion.Lerp(transform.rotation, lookTarget, 0.25f);
-
-            // 캐릭터의 위치와 목표 위치의 거리가 0.05f 보다 큰 동안만 이동
-            move = (transform.position - destPos).magnitude > 0.05f;
+            moveZ -= 1f;
         }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveX -= 1f;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveX += 1f;
+        }
+
+        transform.Translate(new Vector3(moveX, 0f, moveZ) * 0.1f);
     }
 
     public float CalDamage() //temp는 전투 중 집중을 통한 일시적 공격력 증가량
@@ -105,8 +86,8 @@ public class Player : MonoBehaviour
         }
         else
         {
+            Destroy(enemy.gameObject);
             Debug.Log("전투 종료!");
-            
         }
     }
 
@@ -121,7 +102,7 @@ public class Player : MonoBehaviour
         }
         else
         {
-            Destroy(enemy);
+            Destroy(enemy.gameObject);
             Debug.Log("전투 종료!");
 
         }
